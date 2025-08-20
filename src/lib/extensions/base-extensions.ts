aaea0fc6f5bbc4ab03c60422556e8337cfbcb7c5
Mats-Dodd <mats.dodd12@gmail.com>
2025-08-19 21:23:38 -0700

    task list

--- src/lib/extensions/base-extensions.ts
+++ src/lib/extensions/base-extensions.ts
@@ -1,10 +1,16 @@
 import StarterKit from "@tiptap/starter-kit"
 import { DiffDeleteMark, DiffExtension, DiffInsertMark } from "./diff-extension"
 import { Extension } from "@tiptap/core"
+import TaskList from "@tiptap/extension-task-list"
+import TaskItem from "@tiptap/extension-task-item"
 
 export const baseExtensions = [
   StarterKit,
   DiffExtension,
   DiffInsertMark,
   DiffDeleteMark,
+  TaskList,
+  TaskItem.configure({
+    nested: true,
+  }),
 ] as Extension[]

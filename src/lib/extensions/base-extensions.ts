34bc64db01b2d031cf386c13203c6147cfaa8afd
Mats-Dodd <mats.dodd12@gmail.com>
2025-08-19 21:17:36 -0700

    rm italoc

--- src/lib/extensions/base-extensions.ts
+++ src/lib/extensions/base-extensions.ts
@@ -1,12 +1,10 @@
 import StarterKit from "@tiptap/starter-kit"
 import { DiffDeleteMark, DiffExtension, DiffInsertMark } from "./diff-extension"
 import { Extension } from "@tiptap/core"
-import Italic from "@tiptap/extension-italic"
 
 export const baseExtensions = [
   StarterKit,
   DiffExtension,
   DiffInsertMark,
   DiffDeleteMark,
-  Italic,
 ] as Extension[]
